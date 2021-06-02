3510dc6984d45021844150cc19f6653cda77dd11
evanrs <me@evanrs.com>
2021-06-02 02:29:40 -0400

    fix: trim types

--- client/tools/trim.ts
+++ client/tools/trim.ts
@@ -1,11 +1,11 @@
 export function trimRecord<T extends Record<string, string>>(values: T): T {
   const acc = {} as any
   for (const k of Object.keys(values)) {
-    acc[k] = trim(values[k]) ?? values[k]
+    acc[k] = trim(values[k])
   }
   return acc as T
 }
 
-export function trim<T>(v?: string): string {
+export function trim(v?: string): string {
   return typeof v === 'string' ? v.replace(/(^\s+)|(\s+$)/g, '') : ''
 }
